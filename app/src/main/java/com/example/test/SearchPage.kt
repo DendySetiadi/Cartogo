8ff71d2796fa7921d3cbfec4fd2a41d0f7d3fa28
mariatyas <mariatyas2003@gmail.com>
2023-12-14 17:57:14 +0700

    dua

--- app/src/main/java/com/example/test/SearchPage.kt
+++ app/src/main/java/com/example/test/SearchPage.kt
@@ -1,5 +1,6 @@
 package com.example.test
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,19 +9,27 @@ import androidx.fragment.app.Fragment
 import com.example.test.databinding.FragmentSearchPageBinding
 import com.google.firebase.FirebaseApp
 
-class SearchPage : Fragment(R.layout.fragment_search_page) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class SearchPage : Fragment() {
 
-        // Initialize Firebase using the application context
-        FirebaseApp.initializeApp(requireContext())
-    }
+    private lateinit var binding: FragmentSearchPageBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_search_page, container, false)
-        return view
+    ): View {
+        binding = FragmentSearchPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize Firebase using the application context
+        FirebaseApp.initializeApp(requireContext())
+
+        binding.lokasi.setOnClickListener {
+            val intent = Intent(requireContext(), Transaksi::class.java)
+            startActivity(intent)
+        }
     }
 }
